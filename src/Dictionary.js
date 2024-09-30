50ecf5359dc66437db1852c2d96c4a009c5ea3db
Khensani <129982152+khensani8@users.noreply.github.com>
2024-09-30 21:51:21 +0200

    added Results tab

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Results from "./Results";
 import axios from "axios";
 import "./Dictionary.css";
 
@@ -8,13 +9,14 @@ export default function Dictionary() {
   function search(event) {
     event.preventDefault();
 
-    // documentation: https://dictionaryapi.dev/
+    // documentation: https://dictionaryapi.dev/ 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
   function handleResponse(response) {
     console.log(response.data[0]);
+    console.log(response.data[0].meanings[0].definitions[0].definition)
   }
 
 
@@ -27,6 +29,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChnage} />
       </form>
+      <Results />
     </div>
   );
 }
