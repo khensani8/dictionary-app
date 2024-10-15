e83b81fb085364b80da55c892d8d3a3d50bc8a7e
Khensani <129982152+khensani8@users.noreply.github.com>
2024-10-15 21:12:48 +0200

    added meaning and results component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -5,20 +5,19 @@ import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function search(event) {
     event.preventDefault();
 
-    // documentation: https://dictionaryapi.dev/ 
+    // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
   function handleResponse(response) {
-    console.log(response.data[0]);
-    console.log(response.data[0].meanings[0].definitions[0].definition)
+    setResults(response.data[0]);
   }
-
 
   function handleKeywordChnage(event) {
     setKeyword(event.target.value);
@@ -29,7 +28,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChnage} />
       </form>
-      <Results />
+      <Results results={results} />
     </div>
   );
 }
