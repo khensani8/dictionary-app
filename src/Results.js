f6daa31a566d5caa72b2afdaa73458f57eb3bea7
Khensani <129982152+khensani8@users.noreply.github.com>
2024-11-09 15:12:57 +0200

    Photo.js file added and changes made to some elements

--- src/Results.js
+++ src/Results.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Meaning from "./Meaning";
 import Phonetic from "./Phonetic";
-import "./Results.css"
+import Meaning from "./Meaning";
+import "./Results.css";
 
 export default function Results(props) {
   if (props.results) {
@@ -19,9 +19,9 @@ export default function Results(props) {
         </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
-            <div key={index}>
+            <section key={index}>
               <Meaning meaning={meaning} />
-            </div>
+            </section>
           );
         })}
       </div>
